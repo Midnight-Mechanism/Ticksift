6c664634569290e051120b4b9550448b85c1c7e3
Justin Sleep <justin@midnightmechanism.com>
2019-09-01 17:07:34 -0500

    Use multiple columns on large screens

--- resources/views/securities/show.blade.php
+++ resources/views/securities/show.blade.php
@@ -11,13 +11,11 @@
                 <select id="select-ticker" multiple="multiple" style="display: none"></select>
             </div>
         </div>
-        <div class="row my-2">
-            <div class="col-12">
+        <div class="row">
+            <div class="col-12 col-lg-6 my-2">
                 <div id="candlestick-chart"></div>
             </div>
-        </div>
-        <div class="row my-2">
-            <div class="col-12">
+            <div class="col-12 col-lg-6 my-2">
                 <div id="correlation-chart"></div>
             </div>
         </div>
