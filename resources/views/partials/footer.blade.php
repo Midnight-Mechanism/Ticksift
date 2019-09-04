ca823279d0ca0432cb17434b316f6e36aaaeb25d
Justin Sleep <justin@midnightmechanism.com>
2019-09-04 17:17:17 -0500

    Fix footer formatting

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,15 @@
 <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
     <div class="container">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="https://midnightmechanism.com">
+            <img style="width: 30px" src="https://midnightmechanism.com/img/logo-inverted-circle.png">
+        </a>
+        <div id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
             <ul class="navbar-nav ml-auto">
-                <a href="https://midnightmechanism.com">
-                    <img style="width: 30px" src="https://midnightmechanism.com/img/logo-inverted-circle.png">
-                </a>
+                <li>
+                    <span>&copy; Midnight Mechanism, LLC {{ date("Y") }}</span>
+                </li>
             </ul>
         </div>
     </div>
