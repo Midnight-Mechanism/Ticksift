d907460c1f3733623d3b4c6ed88fd92e54e52e61
Justin Sleep <justin@midnightmechanism.com>
2019-09-26 14:25:17 -0500

    Replace SVG logo with PNG

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="/images/ticksift.svg">
+            <img style="height: 30px" src="/images/ticksift.png">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                                                                             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
