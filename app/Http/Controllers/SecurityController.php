9b98d27e3b249bf034904a4d6fa60ca11dd9e699
lagbox <lagbox@gmail.com>
2019-10-09 01:34:51 -0400

    Increase performance of Momentum data retrieval #14

--- app/Http/Controllers/SecurityController.php
+++ app/Http/Controllers/SecurityController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Security;
 use App\Models\Price;
+use App\Models\Security;
+use DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class SecurityController extends Controller
 {
@@ -23,11 +25,7 @@ class SecurityController extends Controller
 
         if ($old_security_ids) {
             $old_securities = Security::whereIn('id', $old_security_ids)
-                ->select(
-                    'id',
-                    'ticker',
-                    'name',
-                )
+                ->select('id', 'ticker', 'name')
                 ->get();
         }
 
@@ -56,74 +54,39 @@ class SecurityController extends Controller
      */
     public function calculateMomentum(Request $request)
     {
-        $dates = explode(' ', $request->input('dates'));
+        $start_date = ($dates = explode(' ', $request->input('dates')))[0];
 
-        $start_date = $dates[0];
-        if (count($dates) > 1) {
-            // date range, e.g. "1995-01-01 to 1995-02-01"
-            $end_date = $dates[2];
-        } else {
-            // single date, e.g. "1995-01-01"
-            $end_date = $dates[0];
-        }
+        $end_date = (count($dates) > 1) ?
+            $dates[2] : // date range, e.g. "1995-01-01 to 1995-02-01"
+            $dates[0]; // single date, e.g. "1995-01-01"
 
         $volume_threshold = $request->input('volume_threshold');
-        $request->session()->put('security_dates', [
-            $start_date,
-            $end_date
-        ]);
-        $request->session()->put('security_volume_threshold', $volume_threshold);
 
-        $min_query = \DB::table('prices')
+        $request->session()->put([
+            'security_dates' => [$start_date, $end_date],
+            'security_volume_threshold' => $volume_threshold,
+        ]);
+
+        $query =  DB::table('prices')
             ->where('volume', '>=', $volume_threshold ?: 0)
-            ->whereBetween('date', [
-                $start_date,
-                $end_date,
-            ])
+            ->whereBetween('date', [$start_date, $end_date])
             ->join('securities', 'prices.security_id', 'securities.id')
-            ->select(\DB::raw('DISTINCT ON (ticker) ticker, date, close'))
+            ->select('ticker', 'date', 'close')
+            ->distinct('ticker')
             ->orderBy('ticker');
 
-        $max_query = clone $min_query;
+        $earliest_prices = (clone $query)->oldest('date')->get();
+        $latest_prices = $query->latest('date')->get();
 
-        $earliest_prices = $min_query
-            ->orderBy('date')
-            ->get();
+        [$winners, $losers] = $this->buildWinnersLosers(
+            $earliest_prices, $latest_prices
+        );
 
-        $latest_prices = $max_query
-            ->orderBy('date', 'desc')
-            ->get();
-
-        $winners = collect([]);
-        $losers = collect([]);
-
-        foreach($earliest_prices as $earliest_price) {
-            $ticker = $earliest_price->ticker;
-            $latest_price = $latest_prices->firstWhere('ticker', $ticker);
-            if (!$latest_price) continue;
-            $coeff = $latest_price->close / $earliest_price->close;
-            if ($coeff >= 1) {
-                $winners->push([
-                    'ticker' => $ticker,
-                    'earliest_close' => $earliest_price->close,
-                    'latest_close' => $latest_price->close,
-                    'increase' => $coeff - 1
-                ]);
-            } else {
-                $losers->push([
-                    'ticker' => $ticker,
-                    'earliest_close' => $earliest_price->close,
-                    'latest_close' => $latest_price->close,
-                    'decrease' => 1 - $coeff
-                ]);
-            }
-        }
-
-        $results['winners'] = $winners->sortByDesc('increase')->values();
-        $results['losers'] = $losers->sortByDesc('decrease')->values();
-        return response()->json($results, 200, [], JSON_NUMERIC_CHECK);
+        return response()->json([
+            'winners' => $winners->sortByDesc('increase')->values(),
+            'losers' => $losers->sortByDesc('decrease')->values(),
+        ], 200, [], JSON_NUMERIC_CHECK);
     }
-
 
     /**
      * Search for the specified resource.
@@ -135,12 +98,9 @@ class SecurityController extends Controller
         $query = $request->input('q');
         $results = Security::where('ticker', 'ILIKE', '%' . $query . '%')
             ->orWhere('name', 'ILIKE', '%' . $query . '%')
-            ->select(
-                'id',
-                'ticker',
-                'name',
-            )
+            ->select('id', 'ticker', 'name')
             ->get();
+
         return response()->json($results);
     }
 
@@ -170,22 +130,55 @@ class SecurityController extends Controller
         $request->session()->put('security_ids', $security_ids);
 
         $prices = [];
+
         foreach ($security_ids as $security_id) {
             $security = Security::findOrFail($security_id);
             $prices[$security->ticker] = $security
-                             ->prices()
-                             ->whereBetween('date', [
-                                 $start_date,
-                                 $end_date,
-                             ])
-                             ->select(
-                                 'date',
-                                 'open',
-                                 'high',
-                                 'low',
-                                 'close',
-                             )->get();
+                ->prices()
+                ->whereBetween('date', [$start_date, $end_date,])
+                ->select(
+                    'date', 'open', 'high', 'low', 'close',
+                )->get();
         }
+
         return response()->json($prices, 200, [], JSON_NUMERIC_CHECK);
+    }
+
+    /**
+     * Build the winners and losers list from prices.
+     *
+     * @param   \Illuminate\Support\Collection  $earliest_prices
+     * @param   \Illuminate\Support\Collection  $latest_prices
+     * @return  array
+     */
+    protected function buildWinnersLosers(Collection $earliestPrices, Collection $latestPrices)
+    {
+        [$winners, $losers] = [collect(), collect()];
+
+        $latest_prices = $latestPrices->keyBy->ticker;
+
+        foreach ($earliestPrices as $earliest_price) {
+            $ticker = $earliest_price->ticker;
+
+            $latest_price = $latest_prices->get($ticker);
+
+            if (! $latest_price) continue;
+
+            $coeff = $latest_price->close / $earliest_price->close;
+
+            $base = [
+                'ticker' => $ticker,
+                'earliest_close' => $earliest_price->close,
+                'latest_close' => $latest_price->close,
+            ];
+
+            if ($coeff >= 1) {
+                $winners->push($base + ['increase' => $coeff - 1]);
+            } else {
+                $losers->push($base + ['decrease' => 1 - $coeff]);
+            }
+        }
+
+        return [$winners, $losers];
     }
 }
