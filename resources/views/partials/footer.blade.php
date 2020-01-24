a68d0951ddbf05f8f3274c028618b7ee09dfc9f8
Justin Sleep <justin@midnightmechanism.com>
2020-01-23 22:30:34 -0600

    Use static URL for MM logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
     <div class="container">
         <a class="navbar-brand" href="https://midnightmechanism.com">
-            <img style="width: 30px" src="https://midnightmechanism.com/img/logo-inverted-circle.png">
+            <img style="width: 30px" src="https://midnightmechanism.com/static/circle_logo.png">
         </a>
         <div id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
