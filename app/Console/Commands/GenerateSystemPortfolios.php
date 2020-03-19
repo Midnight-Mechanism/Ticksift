5d445c2f4a6cd891e852c83e80025cf370e55ff0
Justin Sleep <justin@midnightmechanism.com>
2020-03-18 23:10:34 -0500

    Add command to generate default system portfolios

---
+++ app/Console/Commands/GenerateSystemPortfolios.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Portfolio;
+use App\Models\Security;
+
+class GenerateSystemPortfolios extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'portfolios:generate-system';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate system portfolios';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $portfolio_faang = Portfolio::firstOrCreate([
+            'name' => 'FAANG',
+        ]);
+        $portfolio_faang->securities()->detach();
+        $portfolio_faang->securities()->attach([
+            Security::where('ticker', 'FB')->first()->id,
+            Security::where('ticker', 'AMZN')->first()->id,
+            Security::where('ticker', 'AAPL')->first()->id,
+            Security::where('ticker', 'NFLX')->first()->id,
+            Security::where('ticker', 'GOOG')->first()->id,
+        ]);
+
+        \Log::info($portfolio_faang);
+    }
+}
