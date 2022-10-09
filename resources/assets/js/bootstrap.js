346f8ebe5d9fa47466f9d2ddc392321511daac8f
Justin Sleep <justin@midnightmechanism.com>
2022-10-08 21:53:38 -0500

    Update dependencies

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -13,15 +13,6 @@ try {
     require('bootstrap');
 } catch (e) {}
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 window.Plotly = require('plotly.js-dist');
 window.dayjs = require('dayjs');
 window.jStat = require('jstat');
