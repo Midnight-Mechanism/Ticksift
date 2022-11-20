d1cc946ddaeaff375fff691bb0c456518eaa1036
Justin Sleep <justin@midnightmechanism.com>
2022-11-19 22:40:45 -0600

    Remove portfolio card until they're re-added

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home(props: any) {
   return (
     <Layout auth={props.auth}>
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="p-10 grid grid-cols-1 md:grid-cols-3 gap-5 justify-items-center">
+        <div className="p-10 grid grid-cols-1 md:grid-cols-2 gap-5 justify-items-center">
           <Card
             link={window.route('securities.explorer')}
             imageLink="/images/landing/explorer.png"
@@ -17,12 +17,6 @@ export default function Home(props: any) {
             imageLink="/images/landing/momentum.png"
             title="Examine Momentum"
             body="Quickly summarize the movement of large-cap stocks."
-          />
-          <Card
-            link={window.route('portfolios.index')}
-            imageLink="/images/landing/portfolios.png"
-            title="Establish Portfolios"
-            body="Create portfolios of securities to watch group performance."
           />
         </div>
       </div>
